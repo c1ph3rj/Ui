747c939995e4f9c838a3a4a95a52738323dfeb80
Jeeva Prakash <jeevarag1016@gmail.com>
2022-12-20 19:18:52 +0530

    Working On Buttons

--- app/src/main/java/com/c1ph3r/ui/Buttons.java
+++ app/src/main/java/com/c1ph3r/ui/Buttons.java
@@ -1,19 +1,28 @@
 package com.c1ph3r.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.content.res.AppCompatResources;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.c1ph3r.ui.databinding.ActivityButtonsBinding;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Buttons extends AppCompatActivity {
 
     int btnSelected;
     private ActivityButtonsBinding BUTTONS;
+    private ArrayList<Integer> SelectedChip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,42 @@ public class Buttons extends AppCompatActivity {
         // Method for Animated radio button.
         customRadioButton();
 
+        // Method for Chip Buttons.
+        chipButton();
+
+    }
+
+    private void chipButton() {
+        BUTTONS.ChipButtons.setOnCheckedStateChangeListener(new ChipGroup.OnCheckedStateChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull ChipGroup group, @NonNull List<Integer> checkedIds) {
+                SelectedChip = new ArrayList<>();
+                SelectedChip.addAll(BUTTONS.ChipButtons.getCheckedChipIds());
+                BUTTONS.CheckedChips.removeAllViews();
+                for(Integer SelectedChipId: SelectedChip){
+                    Chip button = findViewById(SelectedChipId);
+                    Chip ChipButton = new Chip(Buttons.this, null, com.google.android.material.R.style.Widget_MaterialComponents_Chip_Choice);
+                    ChipButton.setText(button.getText());
+                    ChipButton.setCloseIcon(AppCompatResources.getDrawable(Buttons.this, R.drawable.close_ic));
+                    ChipButton.setChecked(true);
+                    BUTTONS.CheckedChips.addView(ChipButton);
+                }
+            }
+        });
+
+        BUTTONS.CheckedChips.setOnCheckedStateChangeListener(new ChipGroup.OnCheckedStateChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull ChipGroup group, @NonNull List<Integer> checkedIds) {
+                SelectedChip = new ArrayList<>();
+                SelectedChip.addAll(BUTTONS.CheckedChips.getCheckedChipIds());
+                for(Integer SelectedChipId: SelectedChip){
+                    Chip button = findViewById(SelectedChipId);
+                    Chip ChipButton = new Chip(Buttons.this, null, com.google.android.material.R.style.Widget_MaterialComponents_Chip_Choice);
+                    ChipButton.setText(button.getText());
+                    BUTTONS.CheckedChips.addView(ChipButton);
+            }
+        }
+    });
     }
 
     private void customRadioButton() {
